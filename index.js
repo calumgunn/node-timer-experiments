0ed65c3551161eab4e83c6d4bfbe63458c89a6db
Calum Gunn <calum@calumgunn.com>
2020-03-30 17:17:07 +0200

    Add BPM prompt

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { Client } = require('node-osc');
 const NanoTimer = require('nanotimer');
 const client = new Client('127.0.0.1', 57120);
+const prompt = require('prompt');
 const timer = new NanoTimer();
 
 let count = 0;
@@ -15,7 +16,7 @@ const bang = () => {
   }
 };
 
-const loopScript = ['200m', '100m', '50m', '5m', '2m'];
+let loopScript = ['200m', '100m', '50m', '5m', '2m'];
 let currentScript = 0;
 
 const loop = (interval) => {
@@ -35,8 +36,30 @@ const loop = (interval) => {
   });
 };
 
+const createLoopScript = (ms) => {
+  return [
+    `${ms}m`,
+    `${ms/2}m`,
+    `${ms/4}m`,
+    `${ms/8}m`,
+    `${ms/16}m`,
+  ];
+}
+
 const start = () => {
-  loop(loopScript[currentScript]);
+  prompt.start();
+  prompt.get(['bpm'], (err, result) => {
+    if (err) {
+      console.warn(err);
+      return process.exit();
+    }
+
+    let bpm = parseInt(result.bpm);
+    const bpmInMs = 60000 / bpm;
+    loopScript = createLoopScript(bpmInMs);
+
+    loop(loopScript[currentScript]);
+  });
 };
 
 start();
